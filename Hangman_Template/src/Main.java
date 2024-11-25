94a886781608b3e87151a2258b980cae5491978a
Peter Costantino <36424513+pcostjr@users.noreply.github.com>
2024-11-25 11:51:40 -0500

    Template Upload
    Ignore words_alpha.txt, this is was from a prototype but I'm leaving it here in case you decide to include some File I/O and want to include the entire English dictionary.

---
+++ Hangman_Template/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+/***
+ * @name Hangman
+ * @description: A simple Java Program that simulates the game "Hangman" using Strings.
+ * @author: YOUR_NAME_HERE
+ * @created:
+ * @last_update:
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // list of words that we'll use
+        String[] words = { "cat", "whale", "bunny", "giraffe", "koala",
+                "lobster", "penguin", "kangaroo", "tarantula", "alligator",
+                "chameleon", "dragonfly", "hummingbird", "hippopotamus",
+                "rhinoceros", "chimpanzee", "hedgehog", "grasshopper",
+                "salamander", "armadillo", "porcupine", "warthog",
+                "scorpion", "anaconda", "orangutan" };
+
+        /* some code goes here */
+
+        while (remaining > 0) {
+            /* main game code goes here */
+    }
+}
